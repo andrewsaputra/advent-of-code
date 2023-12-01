736ad89803205863ccab22f2f58e9133c65d189f
Andrew Saputra <andrew.saputra@gmail.com>
2023-12-01 13:55:37 +0700

    move inputs file

--- 2023/day_01/main.go
+++ 2023/day_01/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	startTime := time.Now().UnixMilli()
 
-	result := solve("inputs/inputs.txt")
+	result := solve("inputs.txt")
 	fmt.Println("Result:", result)
 
 	duration := time.Now().UnixMilli() - startTime
