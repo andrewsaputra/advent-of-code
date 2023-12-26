9a0722dbf22e49596941a211f5aa19a636e51f93
Andrew Saputra <andrew.saputra@gmail.com>
2023-12-26 18:25:38 +0700

    optimize AoC 2023 - Day 24

--- 2023/day_24/main.go
+++ 2023/day_24/main.go
@@ -91,16 +91,13 @@ func solvePart2(items []Item) int64 {
 		for _, rdy := range rockDys {
 			for _, rdz := range rockDzs {
 
-				//use line equation to infer rock position from 2 existing points
 				for i := 0; i < n-1; i++ {
 					for j := i + 1; j < n; j++ {
 						a, b := items[i], items[j]
 
-						ma := (a.dy - rdy) / (a.dx - rdx)
-						mb := (b.dy - rdy) / (b.dx - rdx)
-
-						ca := a.y - ma*a.x
-						cb := b.y - mb*b.x
+						//line equations from rock to target points
+						ma, ba := lineEquation2D(a.x, a.y, a.dx-rdx, a.dy-rdy)
+						mb, bb := lineEquation2D(b.x, b.y, b.dx-rdx, b.dy-rdy)
 
 						rock := Item{
 							dx: rdx,
@@ -108,27 +105,29 @@ func solvePart2(items []Item) int64 {
 							dz: rdz,
 						}
 
-						rock.x = (cb - ca) / (ma - mb)
-						rock.y = ma*rock.x + ca
+						//rock.y = ma * rock.x + ba = mb * rock.x + bb
+						rock.x = (bb - ba) / (ma - mb)
+						rock.y = ma*rock.x + ba
 
+						//rock.x = a.x + time * (a.dx - rdx)
 						time := (rock.x - a.x) / (a.dx - rdx)
 						rock.z = a.z + time*(a.dz-rdz)
 
 						//test whether rock does collide with all points
 						valid := true
-						for k := 0; k < n; k++ {
-							if _, ok := hasCollission(rock, items[k]); !ok {
+						for _, test := range items {
+							if _, ok := hasCollission(rock, test); !ok {
 								valid = false
 								break
 							}
 						}
 
 						if valid {
-							//there can be 2 valid total results, each negative and positive
-							//take the positive one
+							//there can be 2 valid rocks
+							//each having negative and positive totals
+							//valid answer is the positive one
 							total := int64(rock.x + rock.y + rock.z)
 							if total > 0 {
-								//fmt.Println(rock)
 								return total
 							}
 						}
@@ -184,20 +183,24 @@ func parseInput(path string) []Item {
 	return result
 }
 
-func findFuturePathCrossing2D(a, b Item) []float64 {
-	//line formula : y = mx + b
-	lineEquation2D := func(item Item) (m float64, b float64) {
-		if item.dx == 0 {
-			return math.MaxInt32, math.MaxInt32
-		}
-
-		m = item.dy / item.dx
-		b = item.y - (m * item.x)
-		return m, b
+/*
+Line Equations :
+y2 - y1 = m * (x2-x1) || m = dy / dx
+y = mx + b || b = y - mx
+*/
+func lineEquation2D(x, y, dx, dy float64) (m float64, b float64) {
+	if dx == 0 {
+		return math.MaxInt32, math.MaxInt32
 	}
 
-	m1, b1 := lineEquation2D(a)
-	m2, b2 := lineEquation2D(b)
+	m = dy / dx
+	b = y - (m * x)
+	return m, b
+}
+
+func findFuturePathCrossing2D(a, b Item) []float64 {
+	m1, b1 := lineEquation2D(a.x, a.y, a.dx, a.dy)
+	m2, b2 := lineEquation2D(b.x, b.y, b.dx, b.dy)
 
 	if m1 == m2 { //parallel lines
 		return nil
@@ -227,16 +230,18 @@ func findFuturePathCrossing2D(a, b Item) []float64 {
 }
 
 func hasCollission(a, b Item) (float64, bool) {
+	//on collission : x2 = a.x + time * a.dx = b.x + time * b.dx
+	const tolerance = 1e-9
 	var time float64
-	if a.dx != b.dx {
+	if math.Abs(a.dx-b.dx) > tolerance {
 		time = (b.x - a.x) / (a.dx - b.dx)
-	} else if a.dy != b.dy {
+	} else if math.Abs(a.dy-b.dy) > tolerance {
 		time = (b.y - a.y) / (a.dy - b.dy)
 	} else {
 		time = (b.z - a.z) / (a.dz - b.dz)
 	}
 
-	//verify
+	//verify collission on all axis
 	ax2 := a.x + (time * a.dx)
 	bx2 := b.x + (time * b.dx)
 	ay2 := a.y + (time * a.dy)
@@ -244,14 +249,9 @@ func hasCollission(a, b Item) (float64, bool) {
 	az2 := a.z + (time * a.dz)
 	bz2 := b.z + (time * b.dz)
 
-	const tolerance = 1e-9
 	if math.Abs(ax2-bx2) > tolerance || math.Abs(ay2-by2) > tolerance || math.Abs(az2-bz2) > tolerance {
 		return 0, false
 	}
-
-	//if ax2 != bx2 || ay2 != by2 || az2 != bz2 {
-	//	return 0, false
-	//}
 
 	return time, true
 }
@@ -259,8 +259,8 @@ func hasCollission(a, b Item) (float64, bool) {
 /*
 Premise :
 Given 2 points that have same velocities on an axis,
-we can guarantee that the past / future distance
-between those 2 points is constant on said axis.
+we can guarantee that the distance between those 2 points
+is always constant on said axis.
 
 Subsequently, we can infer that a valid rock speed which can
 collide with both points on that axis will satisfy the equation :
@@ -277,27 +277,27 @@ func calculatePossibleVelocities(velocities map[float64][]float64) []float64 {
 
 	for vel, pos := range velocities {
 		n := len(pos)
-		if n != 2 {
+		if n < 2 {
 			continue
 		}
 
-		newDvs := []float64{}
-		a, b := pos[0], pos[1]
-		for _, dv := range rockDvs {
-			if dv == vel {
-				continue
-			}
+		for i := 0; i < n-1; i++ {
+			for j := i + 1; j < n; j++ {
+				a, b := pos[i], pos[j]
+				newDvs := []float64{}
+				for _, dv := range rockDvs {
+					if dv == vel || int64(a-b)%int64(vel-dv) != 0 {
+						continue
+					}
 
-			if int64(a-b)%int64(vel-dv) != 0 {
-				continue
-			}
+					newDvs = append(newDvs, dv)
+				}
 
-			newDvs = append(newDvs, dv)
+				rockDvs = newDvs
+			}
 		}
-
-		rockDvs = newDvs
 	}
 
-	//fmt.Println("dvs", rockDvs)
+	//fmt.Println("rockDvs", rockDvs)
 	return rockDvs
 }
