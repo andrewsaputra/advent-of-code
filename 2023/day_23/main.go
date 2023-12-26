0b272381241dbc14e4dc37de17e2064c2f8156f5
Andrew Saputra <andrew.saputra@gmail.com>
2023-12-27 00:48:17 +0700

    AoC 2023 - Day 23 (Part 2)

--- 2023/day_23/main.go
+++ 2023/day_23/main.go
@@ -12,13 +12,13 @@ import (
 func main() {
 	startTime := time.Now().UnixMilli()
 
-	grid := parseInput("inputs.txt")
+	grid, src, dest := parseInput("inputs.txt")
 
-	res1 := solvePart1(grid)
+	res1 := solvePart1(grid, src, dest)
 	fmt.Println("Part 1:", res1)
 
-	//res2 := solvePart2(grid, start)
-	//fmt.Println("Part 2:", res2)
+	res2 := solvePart2(grid, src, dest)
+	fmt.Println("Part 2:", res2)
 
 	fmt.Printf("Duration : %vms", time.Now().UnixMilli()-startTime)
 }
@@ -26,10 +26,10 @@ func main() {
 type Item struct {
 	Row   int
 	Col   int
-	Steps int
+	Steps int64
 }
 
-func parseInput(path string) [][]byte {
+func parseInput(path string) ([][]byte, []int, []int) {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Panic(err)
@@ -45,75 +45,140 @@ func parseInput(path string) [][]byte {
 		grid = append(grid, []byte(line))
 	}
 
-	return grid
+	src := []int{
+		0,
+		strings.Index(string(grid[0]), "."),
+	}
+
+	lastRowIdx := len(grid) - 1
+	dest := []int{
+		lastRowIdx,
+		strings.Index(string(grid[lastRowIdx]), "."),
+	}
+
+	return grid, src, dest
 }
 
-func solvePart1(grid [][]byte) int {
-	numRows, numCols := len(grid), len(grid[0])
-	start := [2]int{0, strings.Index(string(grid[0]), ".")}
-	end := [2]int{numRows - 1, strings.Index(string(grid[numRows-1]), ".")}
+func solvePart1(grid [][]byte, src []int, dest []int) int64 {
+	return longestPathWithSlopes(grid, src, dest)
+}
 
-	visited := make([][]bool, numRows)
-	maxSteps := make([][]int, numRows)
+func solvePart2(grid [][]byte, src []int, dest []int) int64 {
+	//optimization so we can remove logic for bounds checking
+	copy := make([][]byte, len(grid))
 	for row := range grid {
-		visited[row] = make([]bool, numCols)
-		maxSteps[row] = make([]int, numCols)
+		copy[row] = append(copy[row], grid[row]...)
 	}
 
-	return longestPath(grid, visited, maxSteps, start, end, 0)
+	copy[src[0]][src[1]] = '#'
+	copy[dest[0]][dest[1]] = '#'
+	src[0]++
+	dest[0]--
+
+	return longestPathNoSlopesNoBoundsChecking(copy, src, dest) + 2
 }
 
-func longestPath(grid [][]byte, visited [][]bool, maxSteps [][]int, start [2]int, end [2]int, steps int) int {
+func longestPathWithSlopes(grid [][]byte, src []int, dest []int) int64 {
 	numRows, numCols := len(grid), len(grid[0])
 
-	row, col := start[0], start[1]
-	var drow, dcol []int
-	if row == end[0] && col == end[1] {
-		return steps
-	}
+	var dfs func(Item, []int, [][]bool) int64
+	dfs = func(item Item, dest []int, visited [][]bool) int64 {
+		if visited[item.Row][item.Col] {
+			return -1
+		}
+		visited[item.Row][item.Col] = true
+		defer func() {
+			visited[item.Row][item.Col] = false
+		}()
 
-	if visited[row][col] {
-		return -1
-	}
-
-	visited[row][col] = true
-	nextSteps := steps + 1
-
-	switch grid[row][col] {
-	case '.':
-		drow = []int{-1, 0, 1, 0}
-		dcol = []int{0, 1, 0, -1}
-	case '^':
-		drow = []int{-1}
-		dcol = []int{0}
-	case '>':
-		drow = []int{0}
-		dcol = []int{1}
-	case 'v':
-		drow = []int{1}
-		dcol = []int{0}
-	case '<':
-		drow = []int{0}
-		dcol = []int{-1}
-	}
-
-	result := -1
-	for i := range drow {
-		nextRow, nextCol := row+drow[i], col+dcol[i]
-		if nextCol < 0 || nextCol >= numCols || nextRow < 0 || nextRow >= numRows || grid[nextRow][nextCol] == '#' {
-			continue
+		if item.Row == dest[0] && item.Col == dest[1] {
+			return item.Steps
 		}
 
-		if nextSteps > maxSteps[nextRow][nextCol] {
-			newVisited := make([][]bool, numRows)
-			for i := range visited {
-				newVisited[i] = append(newVisited[i], visited[i]...)
+		var drow, dcol []int
+		switch grid[item.Row][item.Col] {
+		case '.':
+			drow = []int{-1, 0, 1, 0}
+			dcol = []int{0, 1, 0, -1}
+		case '^':
+			drow = []int{-1}
+			dcol = []int{0}
+		case '>':
+			drow = []int{0}
+			dcol = []int{1}
+		case 'v':
+			drow = []int{1}
+			dcol = []int{0}
+		case '<':
+			drow = []int{0}
+			dcol = []int{-1}
+		default:
+			panic("unknown cell")
+		}
+
+		result := int64(-1)
+		nextSteps := item.Steps + 1
+		for i := range drow {
+			nextRow := item.Row + drow[i]
+			nextCol := item.Col + dcol[i]
+			if nextCol < 0 || nextCol >= numCols || nextRow < 0 || nextRow >= numRows || grid[nextRow][nextCol] == '#' {
+				continue
 			}
 
-			maxSteps[nextRow][nextCol] = nextSteps
-			result = max(result, longestPath(grid, newVisited, maxSteps, [2]int{nextRow, nextCol}, end, nextSteps))
+			result = max(result, dfs(Item{nextRow, nextCol, nextSteps}, dest, visited))
+
 		}
+
+		return result
 	}
 
-	return result
+	visited := make([][]bool, numRows)
+	for row := range visited {
+		visited[row] = make([]bool, numCols)
+	}
+
+	return dfs(Item{src[0], src[1], 0}, dest, visited)
+}
+
+func longestPathNoSlopesNoBoundsChecking(grid [][]byte, src []int, dest []int) int64 {
+	numRows, numCols := len(grid), len(grid[0])
+	drow := []int{-1, 0, 1, 0}
+	dcol := []int{0, 1, 0, -1}
+
+	var dfs func(Item, []int, [][]bool) int64
+	dfs = func(item Item, dest []int, visited [][]bool) int64 {
+		if visited[item.Row][item.Col] {
+			return -1
+		}
+		visited[item.Row][item.Col] = true
+		defer func() {
+			visited[item.Row][item.Col] = false
+		}()
+
+		if item.Row == dest[0] && item.Col == dest[1] {
+			return item.Steps
+		}
+
+		result := int64(-1)
+		nextSteps := item.Steps + 1
+		for i := range drow {
+			nextRow := item.Row + drow[i]
+			nextCol := item.Col + dcol[i]
+			if grid[nextRow][nextCol] == '#' {
+				continue
+			}
+
+			result = max(result, dfs(Item{nextRow, nextCol, nextSteps}, dest, visited))
+
+		}
+
+		return result
+	}
+
+	visited := make([][]bool, numRows)
+	for row := range visited {
+		visited[row] = make([]bool, numCols)
+	}
+
+	return dfs(Item{src[0], src[1], 0}, dest, visited)
 }
