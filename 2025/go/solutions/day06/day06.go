ef90c1696e54f1c724694e74c78c435a503c3e44
Andrew Saputra <andrew.saputra@gmail.com>
2025-12-06 22:31:52 +0700

    advent of code 2025 - day06 optimization

--- 2025/go/solutions/day06/day06.go
+++ 2025/go/solutions/day06/day06.go
@@ -60,72 +60,52 @@ func solvePart1(path string) int64 {
 	return result
 }
 
-type Sign struct {
-	Sign rune
-	Idx  int
-}
-
 func solvePart2(path string) int64 {
-	var nums [][]string
-	var signs []Sign
-
-	inputLines := helper.ReadLines(path)
-	for idx, val := range inputLines[len(inputLines)-1] {
-		if val == ' ' {
-			continue
+	var nums [][]byte
+	var signs []string
+	for _, line := range helper.ReadLines(path) {
+		if line[0] == '+' || line[0] == '*' {
+			signs = strings.Fields(line)
+			break
 		}
 
-		signs = append(signs, Sign{Sign: val, Idx: idx})
-	}
-
-	for i := 0; i < len(inputLines)-1; i++ {
-		line := inputLines[i]
-
-		var tmp []string
-		for idx, sign := range signs {
-			if idx == len(signs)-1 {
-				tmp = append(tmp, line[sign.Idx:])
-				continue
-			}
-
-			tmp = append(tmp, line[sign.Idx:signs[idx+1].Idx-1])
-		}
-
-		nums = append(nums, tmp)
+		nums = append(nums, []byte(line))
 	}
 
 	numRows, numCols := len(nums), len(nums[0])
-
+	signIdx := len(signs) - 1
 	var result int64
-	for col := range numCols {
-		sign := signs[col].Sign
-		lenStr := len(nums[0][col])
-		var tmpResult int64
-		for i := lenStr - 1; i >= 0; i-- {
-			var tmpVal int64
-			for row := range numRows {
-				val := nums[row][col][i]
-				if val == ' ' {
-					continue
-				}
-
-				tmpVal = tmpVal*10 + int64(val-'0')
+	var tmpResult int64
+	for col := numCols - 1; col >= 0; col-- {
+		var tmpVal int64
+		for row := range numRows {
+			val := nums[row][col]
+			if val == ' ' {
+				continue
 			}
 
-			if tmpResult == 0 {
-				tmpResult = tmpVal
-			} else {
-				switch sign {
-				case '+':
-					tmpResult += tmpVal
-				case '*':
-					tmpResult *= tmpVal
-				}
-			}
+			tmpVal = tmpVal*10 + int64(val-'0')
 		}
 
-		result += tmpResult
+		if tmpVal == 0 {
+			signIdx--
+			result += tmpResult
+			tmpResult = 0
+			continue
+		}
+
+		if tmpResult == 0 {
+			tmpResult = tmpVal
+		} else {
+			switch signs[signIdx] {
+			case "+":
+				tmpResult += tmpVal
+			case "*":
+				tmpResult *= tmpVal
+			}
+		}
 	}
 
+	result += tmpResult
 	return result
 }
